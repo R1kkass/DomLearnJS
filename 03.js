80d491b4e1be9c0c18293f23585fba8945d836b2
Your Name <you@example.com>
2023-03-14 00:28:00 +0300

    commit

---
+++ 03.js
@@ -0,0 +1,9 @@
+let table = document.querySelector('#age-table')
+
+let label = document.querySelectorAll('label')
+
+let row = document.querySelector('table').rows[0].cells[0]
+
+let form  = document.querySelector('form[name="search"]')
+
+let inp = document.querySelectorAll('input')[document.querySelectorAll('input').length-1]
